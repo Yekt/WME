046ef0082ac72fc368f1a6618a5c8b14b5223f8a
JeanReload <35660032+JeanReload@users.noreply.github.com>
2018-11-04 23:12:31 +0100

    header finished

--- Aufgabe1/js/script.js
+++ Aufgabe1/js/script.js
@@ -1,12 +1,16 @@
-window.onscroll = function() {myFunction()};
+/*
+Javascriptvariante für Stickynavigation. Erklärung im layout.css
+
+window.onscroll = function() {myFunction()};  
 
 var header = document.getElementById("header");
 var sticky = header.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > sticky) {
     header.classList.add("sticky")
   } else {
     header.classList.remove("sticky");
   }
 }
+*/
