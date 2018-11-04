9f29a37221a90dbde18fe70e28c690e3c2e49b40
JeanReload <35660032+JeanReload@users.noreply.github.com>
2018-11-05 00:40:31 +0100

    sort function

--- Aufgabe1/js/script.js
+++ Aufgabe1/js/script.js
@@ -1,5 +1,5 @@
 /*
-Javascriptvariante für Stickynavigation. Erklärung im layout.css
+Javascriptoption for Stickynavigation. Further explination in layout.css
 
 window.onscroll = function() {myFunction()};  
 
@@ -14,3 +14,51 @@ function myFunction() {
   }
 }
 */
+
+function sortTableup() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table1");
+  switching = true;
+  
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+function sortTabledown() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table1");
+  switching = true;
+  
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
