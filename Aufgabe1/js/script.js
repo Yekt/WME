beb4fb84e2fea71762a1558cbac7f14983d02112
Lukas <l-sauter@web.de>
2018-11-05 01:03:08 +0100

    js

--- Aufgabe1/js/script.js
+++ Aufgabe1/js/script.js
@@ -8,9 +8,91 @@ var sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky")
+    header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 }
 */
+
+var c2 = true;
+var c3 = true;
+var c4 = true;
+var c5 = true;
+var c6 = true;
+
+
+function hideColumn2() {
+	var column = document.getElementsByClassName("column2");
+	var head = document.getElementById("column2");
+    if(c2){
+		column.classList.add("hidden");
+		head.classList.add("hidden");
+		c2 = false;
+	}
+	else{
+		column.classList.remove("hidden");
+		head.classList.remove("hidden");
+		c2 = true;
+	}
+}
+
+function hideColumn3() {
+    var column = document.getElementsByClassName("column3");
+	var head = document.getElementById("column3");
+    if(c2){
+		column.classList.add("hidden");
+		head.classList.add("hidden");
+		c2 = false;
+	}
+	else{
+		column.classList.remove("hidden");
+		head.classList.remove("hidden");
+		c2 = true;
+	}
+}
+
+function hideColumn4() {
+    var column = document.getElementsByClassName("column4");
+	var head = document.getElementById("column4");
+    if(c2){
+		column.classList.add("hidden");
+		head.classList.add("hidden");
+		c2 = false;
+	}
+	else{
+		column.classList.remove("hidden");
+		head.classList.remove("hidden");
+		c2 = true;
+	}
+}
+
+function hideColumn5() {
+    var column = document.getElementsByClassName("column5");
+	var head = document.getElementById("column5");
+    if(c2){
+		column.classList.add("hidden");
+		head.classList.add("hidden");
+		c2 = false;
+	}
+	else{
+		column.classList.remove("hidden");
+		head.classList.remove("hidden");
+		c2 = true;
+	}
+}
+
+function hideColumn6() {
+    var column = document.getElementsByClassName("column6");
+	var head = document.getElementById("column6");
+    if(c2){
+		column.classList.add("hidden");
+		head.classList.add("hidden");
+		c2 = false;
+	}
+	else{
+		column.classList.remove("hidden");
+		head.classList.remove("hidden");
+		c2 = true;
+	}
+}
