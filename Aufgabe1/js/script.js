83963b8d93e050446a5b4e08d7c1ea444aa4bb8f
JeanReload <35660032+JeanReload@users.noreply.github.com>
2018-11-05 01:02:31 +0100

    sort buttons

--- Aufgabe1/js/script.js
+++ Aufgabe1/js/script.js
@@ -1,5 +1,6 @@
 /*
 Javascriptoption for Stickynavigation. Further explination in layout.css
+https://www.w3schools.com/howto/howto_js_sticky_header.asp
 
 window.onscroll = function() {myFunction()};  
 
@@ -8,13 +9,14 @@ var sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky")
+    header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 }
 */
 
+/*https://www.w3schools.com/howto/howto_js_sort_table.asp*/
 function sortTableup() {
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("table1");
