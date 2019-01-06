a74a1572777586ec7f2ef0ee51a517a7ff1827cc
Lukas <l-sauter@web.de>
2019-01-06 22:46:55 +0100

    bla

--- Aufgabe3_Team18/public/assets/js/ajax.js
+++ Aufgabe3_Team18/public/assets/js/ajax.js
@@ -1,12 +1,24 @@
-$.ajax({
-	type: 'GET',
-	url: 'http://localhost:3000/json',
-	async: true,
-	success: function(data) {
-		alert('success');
-		console.log(data);
-	}, error: function(jqXHR, text, err) {
-		alert('There was an error trying to load the data.');
+function getJson(path){
+	$.ajax({
+		type: 'GET',
+		url: 'http://localhost:3000' + path,
+		async: true,
+		success: function(data) {
+			alert('success');
+			console.log(data);
+			fillTable(data);
+		}, error: function(jqXHR, text, err) {
+			alert('There was an error trying to load the data.');
+		}
+	});
+}
+
+function fillTable(data){
+	for (row = 0; row < data.length; row++) {
+		for (col = 0; col < data[row].length; col++) {
+			
+			document.write();
+			
+		}
 	}
-});
-//alert('TEST ALERT');
+}
